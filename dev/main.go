9bc3257ed8648879ebe4c8a09d2b16ad796b3974
Evan Colwell <ercolwell@gmail.com>
2024-03-15 10:18:42 +1100

    Add keyvault gets

--- dev/main.go
+++ dev/main.go
@@ -183,7 +183,7 @@ func listSubscriptions(token azure.MultiAuthToken) ([]azure.FetchedSubscription,
 	urlString := "https://management.azure.com/subscriptions?api-version=2022-12-01"
 	req, err := http.NewRequest(http.MethodGet, urlString, nil)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Bearer "+token.TokenData.Token)
@@ -208,18 +208,107 @@ func listSubscriptions(token azure.MultiAuthToken) ([]azure.FetchedSubscription,
 	var subsList azure.SubsReqResBody
 	json.Unmarshal(responseBody, &subsList)
 	subsList.UpdateTenantName(token.TenantName)
-	lib.MarshalAndPrintJson(subsList.Value)
+	// lib.MarshalAndPrintJson(subsList.Value)
 
 	return subsList.Value, nil
 }
 
 func main() {
 
-	config := lib.GetCldConfig(lib.CldConfigOptions{})
+	// config := lib.GetCldConfig(lib.CldConfigOptions{})
 	// sub, err := azure.GetActiveSub()
 	// lib.CheckFatalError(err)
 
+	// var allSecrets
+
 	// config.AddAzureTenant("e9f4bce2-7308-461a-91ce-3f663d079f47", "FakeTest")
+	tokens, err := azure.GetAllTenantTokens(azure.AzureRequestOptions{})
+	// fmt.Println(tokens)
+	lib.CheckFatalError(err)
+
+	for _, tenant := range tokens {
+		subs, err := listSubscriptions(tenant)
+		lib.CheckFatalError(err)
+		for _, sub := range subs {
+			lib.PrintSrcLoc("getting kvs")
+			GetAllSubscriptionKeyvaults(tenant, sub.ID, sub.DisplayName)
+		}
+	}
+}
+
+type KeyVault struct {
+	ID         string `json:"id"`
+	Location   string `json:"location"`
+	Name       string `json:"name"`
+	Properties struct {
+		AccessPolicies               []any  `json:"accessPolicies"`
+		EnableRbacAuthorization      bool   `json:"enableRbacAuthorization"`
+		EnableSoftDelete             bool   `json:"enableSoftDelete"`
+		EnabledForDeployment         bool   `json:"enabledForDeployment"`
+		EnabledForDiskEncryption     bool   `json:"enabledForDiskEncryption"`
+		EnabledForTemplateDeployment bool   `json:"enabledForTemplateDeployment"`
+		ProvisioningState            string `json:"provisioningState"`
+		PublicNetworkAccess          string `json:"publicNetworkAccess"`
+		Sku                          struct {
+			Family string `json:"family"`
+			Name   string `json:"name"`
+		} `json:"sku"`
+		SoftDeleteRetentionInDays float64 `json:"softDeleteRetentionInDays"`
+		TenantID                  string  `json:"tenantId"`
+		VaultURI                  string  `json:"vaultUri"`
+	} `json:"properties"`
+	SystemData struct {
+		CreatedAt          time.Time `json:"createdAt"`
+		CreatedBy          string    `json:"createdBy"`
+		CreatedByType      string    `json:"createdByType"`
+		LastModifiedAt     time.Time `json:"lastModifiedAt"`
+		LastModifiedBy     string    `json:"lastModifiedBy"`
+		LastModifiedByType string    `json:"lastModifiedByType"`
+	} `json:"systemData"`
+	Tags struct{} `json:"tags"`
+	Type string   `json:"type"`
+}
+
+type GetSubscriptionKeyvaultsResponse struct {
+	NextLink string        `json:"nextLink"`
+	Value    []interface{} `json:"value"`
+}
+
+func GetAllSubscriptionKeyvaults(token azure.MultiAuthToken, subscriptionId string, subscriptionName string) {
+	var allKeyvaults interface{}
+	// urlString := "https://management.azure.com" + subscriptionId + "/resources?$filter=resourceType eq 'Microsoft.KeyVault/vaults'&api-version=2015-11-01" // resources query
+	urlString := "https://management.azure.com" + subscriptionId + "/providers/Microsoft.KeyVault/vaults?api-version=2022-07-01" // keyvault api
+	// fmt.Println(subscriptionId)
+	// fmt.Println(urlString)
+
+	lib.PrintSrcLoc(subscriptionName)
+	req, err := http.NewRequest(http.MethodGet, urlString, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+token.TokenData.Token)
+
+	res, err := http.DefaultClient.Do(req)
+	lib.CheckFatalError(err)
+
+	responseBody, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
+
+	fmt.Println(string(responseBody))
+
+	var unmarshedBody GetSubscriptionKeyvaultsResponse
+
+	json.Unmarshal(responseBody, unmarshedBody)
+
+	for _, kv := range unmarshedBody.Value {
+		allKeyvaults = append(allKeyvaults, kv)
+		fmt.Println(kv)
+	}
+
+	// lib.MarshalAndPrintJson(allKeyvaults)
+
+	fmt.Println(allKeyvaults)
 
 }
 
@@ -255,7 +344,7 @@ func GetVmDetails(options azure.AzureRequestOptions, token string) {
 		log.Fatal("Error fetching LA Workspace Tables: ", string(responseBody))
 	}
 	if err != nil {
-		log.Fatal(err)
+		lib.CheckFatalError(err)
 	}
 	defer res.Body.Close()
 
@@ -265,29 +354,3 @@ func GetVmDetails(options azure.AzureRequestOptions, token string) {
 	// vmDetails := responseBodyUnmarshal.Value
 	// return vmDetails
 }
-
-// func main() {
-// 	var (
-// 		allSubscriptions []azure.FetchedSubscription
-// 		wg               sync.WaitGroup
-// 		mut              sync.Mutex
-// 	)
-// 	_ = allSubscriptions
-// 	tenantTokens, err := azure.GetAllTenantTokens(azure.GetAllTenantTokenOptions{})
-// 	_ = tenantTokens
-// 	lib.CheckFatalError(err)
-
-// 	for _, tt := range tenantTokens {
-// 		wg.Add(1)
-// 		go func() {
-// 			tenantSubscriptions, err := listSubscriptions(tt)
-// 			lib.CheckFatalError(err)
-// 			mut.Lock()
-// 			allSubscriptions = append(allSubscriptions, tenantSubscriptions...)
-// 			mut.Unlock()
-// 			wg.Done()
-// 		}()
-
-// 	}
-// 	wg.Wait()
-// }
