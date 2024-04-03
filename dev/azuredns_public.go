6accf40ad05615944c2806a8091363ceae8f4fe6
Evan Colwell <ercolwell@gmail.com>
2024-04-04 10:55:25 +1100

    Testing proxy config

--- dev/azuredns_public.go
+++ dev/azuredns_public.go
@@ -1,4 +1,4 @@
-package azuredns
+package main
 
 import (
 	"context"
--- dev/main.go
+++ dev/main.go
@@ -2,14 +2,46 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/jercle/azg/lib"
 	"golang.org/x/sys/windows/registry"
 )
 
 func main() {
-	GetProxySettings()
+	// GetProxySettings()
+	// RemoveProxyConfig()
+	// GetProxySettings()
+
+	confLocation := lib.InitConfig(&lib.CldConfigOptions{})
+	fmt.Println(confLocation)
+
+	// k, err := registry.OpenKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings`, registry.READ)
+	// lib.CheckFatalError(err)
+
+	// defer k.Close()
+
+	// subKeyNames, err := k.ReadSubKeyNames(0)
+	// lib.CheckFatalError(err)
+
+	// valueNames, err := k.ReadValueNames(0)
+	// lib.CheckFatalError(err)
+
+	// fmt.Println("subKeyNames")
+	// subKeyBytes, _ := json.MarshalIndent(subKeyNames, "", "  ")
+	// fmt.Println(string(subKeyBytes))
+	// fmt.Println("valueNames")
+	// valueBytes, _ := json.MarshalIndent(valueNames, "", "  ")
+	// fmt.Println(string(valueBytes))
+	// fmt.Println(valueNames)
+
+	// proxyServer, _, err := k.GetStringValue("ProxyServer")
+
+}
+func SetProxySettings(config string) {
+	k, err := registry.OpenKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings`, registry.WRITE)
+	lib.CheckFatalError(err)
+	defer k.Close()
+
 }
 
 func GetProxySettings() {
@@ -18,15 +50,30 @@ func GetProxySettings() {
 	defer k.Close()
 
 	proxyServer, _, err := k.GetStringValue("ProxyServer")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Current proxy settings: ", proxyServer)
+	lib.CheckFatalError(err)
+	proxyEnable, _, err := k.GetIntegerValue("ProxyEnable")
+	lib.CheckFatalError(err)
+	proxyOverride, _, err := k.GetStringValue("ProxyOverride")
+	lib.CheckFatalError(err)
+
+	fmt.Println("Current proxy server settings:", proxyServer)
+	fmt.Println("Current proxy enabl settings:", proxyEnable)
+	fmt.Println("Current proxy enabl settings:", proxyOverride)
 }
 
 func RemoveProxyConfig() {
-	k, err := registry.OpenKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings`, registry.QUERY_VALUE)
+	k, err := registry.OpenKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings`, registry.SET_VALUE)
 	lib.CheckFatalError(err)
 	defer k.Close()
 
+	// k.DeleteValue("ProxyServer")
+
+	k.SetStringValue("ProxyServer", "")
+
+	// k.
+
 }
+
+// func GetRegistryValues
+// *.apetp.gov.au;172.*;<local>
+// *.apetp.gov.au;172.*;localhost;127.0.0.;10.;192.168.*;*.apetp.gov.au;*.azure.net;*.azure.com;*.windows.net;*.visualstudio.com;*.microsoft.com
