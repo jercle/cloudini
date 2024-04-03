c9db462e0a669e8fd853de94281ca51268910a70
Evan Colwell <ercolwell@gmail.com>
2024-04-04 09:00:24 +1100

    Add ProxyConfig type

--- lib/typesCld.go
+++ lib/typesCld.go
@@ -1,8 +1,15 @@
 package lib
 
 type CldConfigRoot struct {
-	CldConfig CldConfig   `json:"cldConfig"`
-	Azure     AzureConfig `json:"azure"`
+	CldConfig   CldConfig              `json:"cldConfig"`
+	Azure       AzureConfig            `json:"azure"`
+	ProxyConfig map[string]ProxyConfig `json:"proxyConfig"`
+}
+
+type ProxyConfig struct {
+	Server  string `json:"server"`
+	Port    string `json:"port"`
+	Enabled bool   `json:"enabled"`
 }
 
 type CldConfig struct {
