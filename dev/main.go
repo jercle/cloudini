d7289d7604b0c4aacf29d58255b0e322e84398d1
Evan Colwell <ercolwell@gmail.com>
2024-04-12 16:51:35 +1000

    Updates

--- dev/main.go
+++ dev/main.go
@@ -1,37 +1,83 @@
+// Citrix Cloud stuff
+
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"time"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 
-	"github.com/jercle/azg/cmd/azure"
 	"github.com/jercle/azg/lib"
 )
 
+type TokenRequestResponse struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   string `json:"expires_in"`
+	TokenType   string `json:"token_type"`
+}
+
+type CitrixResourceLocationResponse struct {
+	Items []CitrixResourceLocationResponseItem `json:"items"`
+}
+
+type CitrixResourceLocationResponseItem struct {
+	ID           string `json:"id"`
+	InternalOnly bool   `json:"internalOnly"`
+	Name         string `json:"name"`
+	ReadOnly     bool   `json:"readOnly"`
+	TimeZone     string `json:"timeZone"`
+}
+
 func main() {
-	startTime := time.Now()
-	config := lib.GetCldConfig(nil)
-	_ = config
-	// tokens, err := azure.GetAllTenantSPTokens(lib.MultiAuthTokenRequestOptions{})
-	// lib.CheckFatalError(err)
-	token, err := azure.GetTenantSPToken(lib.MultiAuthTokenRequestOptions{})
-	lib.CheckFatalError(err)
-	// _ = tokens
-	_ = token
-	firewallPolicyName := ""
-	// azureFirewallName := ""
-	subscriptionId := ""
-	resourceGroupName := ""
-	fwpRuleCollectionName := ""
-	urlString := "https://management.azure.com/subscriptions/" + subscriptionId + "/resourceGroups/" + resourceGroupName + "/providers/Microsoft.Network/firewallPolicies/" + firewallPolicyName + "/ruleCollectionGroups/" + fwpRuleCollectionName + "?api-version=2023-09-01"
-	// urlString := "https://management.azure.com/subscriptions/" + subscriptionId + "/resourceGroups/" + resourceGroupName + "/providers/Microsoft.Network/azureFirewalls/" + azureFirewallName + "?api-version=2023-09-01"
+	token := GetCitrixCloudToken()
 
-	resp, err := azure.HttpGet(urlString, *token)
+}
+
+func GetCitrixCloudResourceLocations(token TokenRequestResponse) {
+	customerId := os.Getenv("CUSTOMER_ID")
+	urlString := "https://registry.citrixworkspacesapi.net/" + customerId + "/resourcelocations"
+
+	req, err := http.NewRequest(http.MethodGet, urlString, nil)
+
+	lib.CheckFatalError(err)
+	req.Header.Add("Authorization", "CwsAuth Bearer="+token.AccessToken)
+
+	res, err := http.DefaultClient.Do(req)
 	lib.CheckFatalError(err)
 
-	fmt.Println(string(resp))
+	responseBody, err := io.ReadAll(res.Body)
+	lib.CheckFatalError(err)
 
-	elapsed := time.Since(startTime)
-	_ = elapsed
-	// fmt.Println(elapsed)
+	fmt.Println(string(responseBody))
+}
+
+func GetCitrixCloudToken() TokenRequestResponse {
+	tokenUrl := "https://api-ap-s.cloud.com/cctrustoauth2/root/tokens/clients"
+
+	tokenReqBody := url.Values{}
+	tokenReqBody.Set("grant_type", "client_credentials")
+	tokenReqBody.Set("client_id", os.Getenv("CLIENT_ID"))
+	tokenReqBody.Set("client_secret", os.Getenv("CLIENT_SECRET"))
+
+	req, err := http.NewRequest(http.MethodPost, tokenUrl, strings.NewReader(tokenReqBody.Encode()))
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	lib.CheckFatalError(err)
+
+	res, err := http.DefaultClient.Do(req)
+	lib.CheckFatalError(err)
+
+	responseBody, err := io.ReadAll(res.Body)
+	lib.CheckFatalError(err)
+
+	var tokenResp TokenRequestResponse
+
+	json.Unmarshal(responseBody, &tokenResp)
+
+	return tokenResp
 }
