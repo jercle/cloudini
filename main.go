33686c6594cc447058436b843d4653aac21a3202
Evan Colwell <ercolwell@gmail.com>
2024-05-13 13:45:11 +1000

    Testing version updates

--- main.go
+++ main.go
@@ -4,10 +4,6 @@ Copyright © 2024 Evan Colwell ercolwell@gmail.com
 package main
 
 import (
-	"flag"
-	"fmt"
-	"time"
-
 	"github.com/jercle/cloudini/cmd"
 	_ "github.com/jercle/cloudini/cmd/ado"
 	_ "github.com/jercle/cloudini/cmd/azure"
@@ -15,38 +11,9 @@ import (
 	_ "github.com/jercle/cloudini/cmd/jira"
 	_ "github.com/jercle/cloudini/cmd/utils"
 	_ "github.com/jercle/cloudini/cmd/web"
-
-	"github.com/carlmjohnson/versioninfo"
-)
-
-var (
-	version = "dev"
-	commit  = "none"
-	date    = "unknown"
 )
 
 func main() {
-	// if installing from source, i.e. not a binary built by `Goreleaser`, this version will not be updated. In that case, use the automagic detection in `versioninfo`
-	if version == "dev" {
-		version = versioninfo.Version
-		commit = versioninfo.Revision
-		date = versioninfo.LastCommit.Format(time.RFC3339)
-	} else {
-		// Goreleaser doesn't prefix with a `v`, which we expect
-		version = "v" + version
-	}
-	fmt.Println("Version:", versioninfo.Version)
-	fmt.Println("Revision:", versioninfo.Revision)
-	fmt.Println("DirtyBuild:", versioninfo.DirtyBuild)
-	fmt.Println("LastCommit:", versioninfo.LastCommit)
-
-	fmt.Println("ShortInfo:", versioninfo.Short())
-
-	versioninfo.AddFlag(nil)
-	flag.Parse()
-	fmt.Println("done")
-
-	cmd.SetVersionInfo(version, commit, date)
+	// defer lib.TimeTrack(time.Now(), "main")
 	cmd.Execute()
-
 }
