c493853a9d71ff3c257f7ba4cfc258d03c14c0aa
Evan Colwell <ercolwell@gmail.com>
2024-06-12 13:04:22 +1000

    Adding vm image version list functionality

--- dev/main.go
+++ dev/main.go
@@ -1,6 +1,9 @@
+// https://learn.microsoft.com/en-us/rest/api/compute/virtual-machine-images/list?view=rest-compute-2024-03-01&tabs=HTTP
+
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -12,34 +15,48 @@ func main() {
 	startTime := time.Now()
 	config := lib.GetCldConfig(nil)
 	_ = config
-
-	var (
-		subscriptionId   = "fdeee0c2-5569-40ea-9ad9-81dd325f6e1e"
-		resourceGroup    = "rg-apcdtqdesktop-aib"
-		galleryName      = "sigapcdtqdesktopaibimages"
-		galleryImageName = "imgdef-apc_oftadtq"
-	)
 	// tokens, err := azure.GetAllTenantSPTokens(lib.MultiAuthTokenRequestOptions{})
 	// lib.CheckFatalError(err)
-	// _ = tokens
-	token, err := azure.GetTenantSPToken(lib.MultiAuthTokenRequestOptions{
-		TenantName: "REDDTQ",
-	})
+	token, err := azure.GetTenantSPToken(lib.MultiAuthTokenRequestOptions{})
 	lib.CheckFatalError(err)
+	// _ = tokens
 	_ = token
 
-	versions := azure.GetGalleryImageVersions(subscriptionId, resourceGroup, galleryName, galleryImageName, *token)
-	// _ = versions
-	// azure.GetGalleryImage(subscriptionId, resourceGroup, galleryName, galleryImageName, *token)
-	// fmt.Println(latest)
-	if len(versions) == 0 {
-		fmt.Println("")
-	} else {
-		latest, _ := versions.Latest()
-		fmt.Println(latest.IncrementPatchVersion())
+	subscriptionId := ""
+	location := "australiaeast"
+	publisherName := "MicrosoftWindowsDesktop"
+	offer := "office-365"
+	skus := "win10-22h2-avd-m365-g2"
+
+	urlString := "https://management.azure.com/subscriptions/" +
+		subscriptionId +
+		"/providers/Microsoft.Compute/locations/" +
+		location + "/publishers/" +
+		publisherName + "/artifacttypes/vmimage/offers/" +
+		offer + "/skus/" +
+		skus + "/versions?api-version=2024-03-01&$orderby=name"
+
+	res, err := azure.HttpGet(urlString, *token)
+	lib.CheckFatalError(err)
+
+	// fmt.Println(string(res))
+
+	var imageList ListVirtualMachineImagesResponse
+	json.Unmarshal(res, &imageList)
+
+	for _, image := range imageList {
+		fmt.Println(image.Name)
 	}
 
 	elapsed := time.Since(startTime)
 	_ = elapsed
 	// fmt.Println(elapsed)
+}
+
+type ListVirtualMachineImagesResponse []VirtualMachineImage
+
+type VirtualMachineImage struct {
+	ID       string `json:"id"`
+	Location string `json:"location"`
+	Name     string `json:"name"`
 }
