d39f69824e67ce289b66ac01d0613df94441728e
Evan Colwell <ercolwell@gmail.com>
2024-06-20 14:36:37 +1000

    Updates, testing m365 endpoint retrieval

--- dev/main.go
+++ dev/main.go
@@ -4,12 +4,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 
 	"github.com/jercle/cloudini/cmd/azure"
 	"github.com/jercle/cloudini/lib"
 )
+
+type Microsoft365Endpoint struct {
+	Category               string   `json:"category"`
+	ExpressRoute           bool     `json:"expressRoute"`
+	ID                     float64  `json:"id"`
+	Ips                    []string `json:"ips,omitempty"`
+	Notes                  string   `json:"notes,omitempty"`
+	Required               bool     `json:"required"`
+	ServiceArea            string   `json:"serviceArea"`
+	ServiceAreaDisplayName string   `json:"serviceAreaDisplayName"`
+	TcpPorts               string   `json:"tcpPorts,omitempty"`
+	UdpPorts               string   `json:"udpPorts,omitempty"`
+	Urls                   []string `json:"urls,omitempty"`
+	Protocols              []string `json:"protocols,omitempty"`
+}
 
 func main() {
 	startTime := time.Now()
@@ -20,38 +36,71 @@ func main() {
 	// token, err := azure.GetTenantSPToken(lib.MultiAuthTokenRequestOptions{})
 	tokenReq, err := azure.GetAllTenantSPTokens(lib.MultiAuthTokenRequestOptions{})
 	lib.CheckFatalError(err)
-	token, err := tokenReq.SelectTenant("RED")
+	token, err := tokenReq.SelectTenant("REDDTQ")
 	lib.CheckFatalError(err)
 	// _ = tokens
 	_ = token
 
-	subscriptionId := ""
-	resourceGroupName := "rg-apcconn"
-	firewallPolicyName := "fwp-apcconn"
-	ruleCollectionGroupName := "fwrulecoll-group-network-microsoft365-allow"
+	webserviceUrl := "https://endpoints.office.com"
+	clientRequestId := ""
+	tenantName := ""
 
-	urlString := "https://management.azure.com/subscriptions/" +
-		subscriptionId +
-		"/resourceGroups/" +
-		resourceGroupName +
-		"/providers/Microsoft.Network/firewallPolicies/" +
-		firewallPolicyName +
-		"/ruleCollectionGroups/" +
-		ruleCollectionGroupName +
-		"?api-version=2023-09-01"
+	// Get the latest versioning data from the Office 365 IP Address and URL web service
+	// urlString := webserviceUrl +
+	// 	"/version/Worldwide?ClientRequestId=" +
+	// 	clientRequestId
 
-	// "https://management.azure.com/subscriptions/" +
-	// 	subscriptionId +
-	// 	"/providers/Microsoft.Compute/locations/" +
-	// 	location + "/publishers/" +
-	// 	publisherName + "/artifacttypes/vmimage/offers/" +
-	// 	offer + "/skus/" +
-	// 	skus + "/versions?api-version=2024-03-01&$orderby=name"
+	// Query the Office 365 IP Address and URL web service for new data
+	urlString := webserviceUrl + "/endpoints/Worldwide?NoIPv6=true&ClientRequestId=" +
+		clientRequestId +
+		"&TenantName=" +
+		tenantName
 
 	res, err := azure.HttpGet(urlString, *token)
 	lib.CheckFatalError(err)
 
-	fmt.Println(string(res))
+	var (
+		endpointList       []Microsoft365Endpoint
+		processedEndpoints []Microsoft365Endpoint
+		networkEndpoints   []Microsoft365Endpoint
+		appEndpoints       []Microsoft365Endpoint
+	)
+
+	// networkEndpoints := []Microsoft365Endpoint{}
+
+	json.Unmarshal(res, &endpointList)
+
+	for _, item := range endpointList {
+		if item.TcpPorts != "" {
+			item.Protocols = append(item.Protocols, "TCP")
+		}
+
+		if item.UdpPorts != "" {
+			item.Protocols = append(item.Protocols, "UDP")
+		}
+
+		// fmt.Println(len(item.Ips))
+
+		if len(item.Ips) != 0 {
+			// fmt.Println(len(item.Ips))
+			networkEndpoints = append(networkEndpoints, item)
+			// jsonstr, _ := json.MarshalIndent(item, "", "  ")
+			// fmt.Println(string(jsonstr))
+		}
+
+		if len(item.Urls) != 0 {
+			// fmt.Println(len(item.Urls))
+			appEndpoints = append(appEndpoints, item)
+		}
+
+		processedEndpoints = append(processedEndpoints, item)
+	}
+
+	jsonStr, err := json.MarshalIndent(processedEndpoints, "", "  ")
+	lib.CheckFatalError(err)
+
+	fmt.Println(string(jsonStr))
+	// fmt.Println(string(res))
 
 	// var imageList ListVirtualMachineImagesResponse
 	// json.Unmarshal(res, &imageList)
@@ -63,12 +112,4 @@ func main() {
 	elapsed := time.Since(startTime)
 	_ = elapsed
 	// fmt.Println(elapsed)
-}
-
-type ListVirtualMachineImagesResponse []VirtualMachineImage
-
-type VirtualMachineImage struct {
-	ID       string `json:"id"`
-	Location string `json:"location"`
-	Name     string `json:"name"`
 }
