aca205108b1fb297d13934f73232617828172804
jercle <ercolwell@gmail.com>
2025-05-16 17:18:26 +1000

    updates

--- cmd/ad/types.go
+++ cmd/ad/types.go
@@ -1,5 +1,7 @@
 package ad
 
+import "time"
+
 type SearchResponse struct {
 	Controls []any `json:"Controls"`
 	Entries  []struct {
@@ -14,28 +16,29 @@ type SearchResponse struct {
 }
 
 type ADUser struct {
-	ObjectClass []string `json:"objectClass"`
-	CN          string   `json:"cn"`
-	Description string   `json:"description"`
-
-	GivenName                     string   `json:"givenName"`
-	Initials                      string   `json:"initials"`
-	DistinguishedName             string   `json:"distinguishedName"`
-	WhenCreated                   string   `json:"whenCreated"`
-	WhenChanged                   string   `json:"whenChanged"`
-	DisplayName                   string   `json:"displayName"`
-	MemberOf                      []string `json:"memberOf"`
-	Name                          string   `json:""`
-	ObjectGUID                    string   `json:"objectGUID"`
-	LastLogon                     string   `json:"lastLogon"`
-	PwdLastSet                    string   `json:"pwdLastSet"`
-	ObjectSid                     string   `json:"objectSid"`
-	AccountExpires                string   `json:"accountExpires"`
-	SAMAccountName                string   `json:"sAMAccountName"`
-	UserPrincipalName             string   `json:"userPrincipalName"`
-	LockoutTime                   string   `json:"lockoutTime"`
-	LastLogonTimestamp            string   `json:"lastLogonTimestamp"`
-	MsDSExternalDirectoryObjectId string   `json:"msDS-ExternalDirectoryObjectId"`
-	Mail                          string   `json:"mail"`
-	MailNickname                  string   `json:"mailNickname"`
+	ObjectClass                   []string  `json:"objectClass"`
+	CN                            string    `json:"cn"`
+	Description                   string    `json:"description"`
+	Domain                        string    `json:"domain"`
+	GivenName                     string    `json:"givenName"`
+	Initials                      string    `json:"initials"`
+	DistinguishedName             string    `json:"distinguishedName"`
+	WhenCreated                   string    `json:"whenCreated"`
+	WhenChanged                   string    `json:"whenChanged"`
+	DisplayName                   string    `json:"displayName"`
+	MemberOf                      []string  `json:"memberOf"`
+	Name                          string    `json:""`
+	ObjectGUID                    string    `json:"objectGUID"`
+	LastLogon                     string    `json:"lastLogon"`
+	PwdLastSet                    string    `json:"pwdLastSet"`
+	ObjectSid                     string    `json:"objectSid"`
+	AccountExpires                string    `json:"accountExpires"`
+	SAMAccountName                string    `json:"sAMAccountName"`
+	UserPrincipalName             string    `json:"userPrincipalName"`
+	LockoutTime                   string    `json:"lockoutTime"`
+	LastLogonTimestamp            string    `json:"lastLogonTimestamp"`
+	MsDSExternalDirectoryObjectId string    `json:"msDS-ExternalDirectoryObjectId"`
+	Mail                          string    `json:"mail"`
+	MailNickname                  string    `json:"mailNickname"`
+	LastDBSync                    time.Time `json:"lastDBSync"`
 }
